a111dfe9c841c62e6ce92f39edbf36812ddaf375
bastien-7 <bastien.terrier7@gmail.com>
2017-11-10 17:11:34 +0100

    etape 10

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+interface Recherche {
+title : string;
+author : string;
+date : string;
+image : string;
+}
+
+const Resultats : Array<Recherche> = [{title:"Titre",author:"author",date:"date",image:"http://www.lorempixel.com/400/200"},
+  { title: "Titre2", author: "author2", date: "date2", image: "http://www.lorempixel.com/400/200" },
+  { title: "Titre3", author: "author3", date: "date3", image: "http://www.lorempixel.com/400/200" }
+];
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  liste: Recherche[] = Resultats;
+  query :string = "";
+  ionInput():void{
+
+    console.log(this.query);
+
+  }
   constructor(public navCtrl: NavController) {
 
   }
