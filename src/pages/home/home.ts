a3fbc1f1b76c756193dcd05bb1e1e0710111d476
bastien-7 <bastien.terrier7@gmail.com>
2017-12-01 14:18:07 +0100

    AJOUT DETAILS

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,10 +8,11 @@ date : string;
 image : string;
 }
 
-const Resultats : Array<Recherche> = [{title:"Titre",author:"author",date:"date",image:"http://www.lorempixel.com/400/200"},
-  { title: "Titre2", author: "author2", date: "date2", image: "http://www.lorempixel.com/400/200" },
-  { title: "Titre3", author: "author3", date: "date3", image: "http://www.lorempixel.com/400/200" }
-];
+const Resultats : Array<Recherche> =[];
+//  =[{ title: "Titre", author: "author", date: "date", image: "http://www.lorempixel.com/400/200" },
+// { title: "Titre2", author: "author2", date: "date2", image: "http://www.lorempixel.com/400/200" },
+// { title: "Titre3", author: "author3", date: "date3", image: "http://www.lorempixel.com/400/200" }
+// ];
 
 @Component({
   selector: 'page-home',
